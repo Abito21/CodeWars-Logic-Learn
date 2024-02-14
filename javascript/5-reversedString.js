8547a19c009cfc094a6d066a02d185561dd7058a
Abito21 <abidjuliant@gmail.com>
2024-02-15 06:36:42 +0700

    Second logic code reversedString

--- javascript/5-reversedString.js
+++ javascript/5-reversedString.js
@@ -7,12 +7,23 @@ Complete the solution so that it reverses the string passed into it.
 */
 
 // First Logic
+// function solution(str){
+//   let result = ""
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i]
+//   }
+//   return result
+// }
+
+// Second Logic
 function solution(str){
-  let result = ""
-  for (let i = str.length - 1; i >= 0; i--) {
-    result += str[i]
-  }
-  return result
+    let splitString = str.split("")
+
+    let reverseArray = splitString.reverse()
+
+    let joinArray = reverseArray.join("")
+
+    return joinArray
 }
 
 console.log(solution("World"))

4bbf5026a100e1963f02d294b508c873cfa0f6f8
Abito21 <abidjuliant@gmail.com>
2024-02-14 17:34:39 +0700

    Second logic code evenOrOdd

--- javascript/3-evenOrOdd.js
+++ javascript/3-evenOrOdd.js
@@ -4,12 +4,15 @@ Create a function that takes an integer as an argument and returns "Even" for ev
 */
 
 // First Logic
-function evenOrOdd(number) {
-    if (number % 2 == 0){
-        return "Even"
-    } else {
-        return "Odd"
-    }
-}
+// function evenOrOdd(number) {
+//     if (number % 2 == 0){
+//         return "Even"
+//     } else {
+//         return "Odd"
+//     }
+// }
+
+// Second Logic
+const evenOrOdd = (number) => number % 2 == 0 ? "Even" : "Odd"
 
 console.log(evenOrOdd(1))

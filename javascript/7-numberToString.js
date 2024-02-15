de3aa750f0b0998a64d5ad0ce0cb2265b63bdf1e
Abito21 <abidjuliant@gmail.com>
2024-02-16 06:00:35 +0700

    Logic code numToString

---
+++ javascript/7-numberToString.js
@@ -0,0 +1,19 @@
+/*
+Problem :
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Example :
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+*/
+
+// First Logic
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+};
+
+console.log(numberToString(-23))

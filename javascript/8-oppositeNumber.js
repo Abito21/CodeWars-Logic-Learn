85987d76acc0d57e0d558f042407dd21959dfc08
Abito21 <abidjuliant@gmail.com>
2024-02-16 06:05:21 +0700

    Logic code oppositeNumber

---
+++ javascript/8-oppositeNumber.js
@@ -0,0 +1,17 @@
+/*
+Problem :
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Example :
+1: -1
+14: -14
+-34: 34
+*/
+
+// First Logic
+function opposite(number) {
+    //your code here
+    return number * -1
+};
+
+console.log(opposite(12.98));

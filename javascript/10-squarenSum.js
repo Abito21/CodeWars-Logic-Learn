a8781a2146c9908a002d42cfb8146dea0d4159c9
Abito21 <abidjuliant@gmail.com>
2024-02-18 06:55:58 +0700

    Third logic code square(n)Sum

--- javascript/10-squarenSum.js
+++ javascript/10-squarenSum.js
@@ -15,8 +15,17 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 // };
 
 // Second Logic
+// function squareSum(numbers){
+//     return numbers.reduce((sum,num) => sum + (num * num), 0);
+// };
+
+// Third Logic
 function squareSum(numbers){
-    return numbers.reduce((sum,num) => sum + (num * num), 0);
+    var sum = 0;
+    numbers.forEach(function(n) {
+        sum += n ** 2
+    });
+    return sum;
 };
 
 console.log(squareSum([1, 5]));

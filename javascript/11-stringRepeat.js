432599800faf551b9b2ba715fec5203d8bf9ee7e
Abito21 <abidjuliant@gmail.com>
2024-02-19 06:15:02 +0700

    Second logic code repeatString recursion

--- javascript/11-stringRepeat.js
+++ javascript/11-stringRepeat.js
@@ -8,13 +8,31 @@ Example :
 */
 
 // First Logic : using while condition
+// function repeatStr (n, s) {
+//     let newString = ""
+//     while (n > 0) {
+//         newString += s
+//         n--
+//     }
+//     return newString;
+// }
+
+// Second Logic : using conditional and recursion
 function repeatStr (n, s) {
-    let newString = ""
-    while (n > 0) {
-        newString += s
-        n--
+    // Step 1 : Check if number is negative and return an empty string if true
+    if (n < 0) {
+        return ""
     }
-    return newString;
+
+    // Step 2 : Check if number is negative and return the string itself if it's the case.
+    if (n === 1) {
+        return s
+    }
+
+    // Step 3 : Use recursion
+    else {
+        return s + repeatStr(n - 1, s)
+    }
 }
 
 console.log(repeatStr(3, "*"))

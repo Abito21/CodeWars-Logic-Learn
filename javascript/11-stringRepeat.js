2285a9f02c440cc3a97dfdf32a0e1aab41f344f3
Abito21 <abidjuliant@gmail.com>
2024-02-19 06:17:30 +0700

    Third logic code repeatString using repeat() ES6 function

--- javascript/11-stringRepeat.js
+++ javascript/11-stringRepeat.js
@@ -18,20 +18,29 @@ Example :
 // }
 
 // Second Logic : using conditional and recursion
+// function repeatStr (n, s) {
+//     // Step 1 : Check if number is negative and return an empty string if true
+//     if (n < 0) {
+//         return ""
+//     }
+
+//     // Step 2 : Check if number is negative and return the string itself if it's the case.
+//     if (n === 1) {
+//         return s
+//     }
+
+//     // Step 3 : Use recursion
+//     else {
+//         return s + repeatStr(n - 1, s)
+//     }
+// }
+
+// Third Logic : Using repeat() function ES6
 function repeatStr (n, s) {
-    // Step 1 : Check if number is negative and return an empty string if true
-    if (n < 0) {
+    if (n > 0) {
+        return s.repeat(n)
+    } else {
         return ""
-    }
-
-    // Step 2 : Check if number is negative and return the string itself if it's the case.
-    if (n === 1) {
-        return s
-    }
-
-    // Step 3 : Use recursion
-    else {
-        return s + repeatStr(n - 1, s)
     }
 }
 

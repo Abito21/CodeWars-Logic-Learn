4f8507f2a0193d6d928b557ece6a1f8afbfd876c
Abito21 <abidjuliant@gmail.com>
2024-02-22 06:18:17 +0700

    Logic code find small integer finder in the array

---
+++ javascript/14-smallInt.js
@@ -0,0 +1,20 @@
+/*
+Problem :
+Given an array of integers your solution should find the smallest integer.
+
+Example :
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+// First Logic
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      const sorted = args.sort((num1, num2) => num1 - num2)
+      return sorted[0]
+    }
+}
+
+let sif = new SmallestIntegerFinder()
+console.log(sif.findSmallestInt([78,56,232,12,8]))

88d1d6aaed15bf3ed67e8deb121bc05e6fe7d70d
Abito21 <abidjuliant@gmail.com>
2024-02-26 08:26:31 +0700

    Sixth logic code string to number using breakdown array

--- javascript/17-string2Number.js
+++ javascript/17-string2Number.js
@@ -41,13 +41,26 @@ Example :
 //     return + str;
 // }
 
-// Fifth Logic : using case plus with character itself
+// Fifth Logic : using case time character with 1 (int)
 // If we have character string and time with 1 (int), the system define the new data type of character is number (it can be int or float).
 // Notes : the string is a standalalone character (number). 
-const stringToNumber = function(str){
-    // put your code here
-    // return typeof(str) // to check data type
-    return str * 1;
+// const stringToNumber = function(str){
+//     // put your code here
+//     // return typeof(str) // to check data type
+//     return str * 1;
+// }
+
+// Sixth Logic : using alternative code
+// the alternative method without parse
+const stringToNumber = function(str, arr = [...str], out = 0){
+    if( arr[0] === '-' ){ arr.shift() }
+    arr.reverse()
+    
+    const nums = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 0: 0}
+    for (let i = 0; i < arr.length; i++){
+        out += nums[arr[i]] * (10**i)
+      }
+    return str[0] === '-' ? -out : out
 }
 
-console.log(stringToNumber("17.55"))
+console.log(stringToNumber("17"))

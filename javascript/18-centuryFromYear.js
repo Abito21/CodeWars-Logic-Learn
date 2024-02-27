a631f4ff0c428312f38a92de4b0c980900861394
Abito21 <abidjuliant@gmail.com>
2024-02-27 11:31:32 +0700

    Fourth logic code century from year change the number to string

--- javascript/18-centuryFromYear.js
+++ javascript/18-centuryFromYear.js
@@ -34,9 +34,36 @@ https://en.wikipedia.org/wiki/Century
 // }
 
 // Third logic : using formula
-function century(year) {
-    // Finish this :)
-    return (year + 99) / 100 | 0;
+// function century(year) {
+//     // Finish this :)
+//     return (year + 99) / 100 | 0;
+// }
+
+// Fourth Logic : change the number to string
+let centuryEnd;
+
+function century(inYear) {
+  let century;
+  const newYear = String(inYear);
+
+  const year = newYear.split('').join('');
+  const len = year.length;
+
+  if (`${year[len - 2]}${year[len - 1]}` === String('00')) {
+    century = Math.round(Number(inYear) / 100);
+  } else
+  if (year.length < 3) {
+    century = 1;
+  } else
+  if (year.length > 4) {
+    century = Math.ceil(Number(inYear) / 100);
+  } else {
+    centuryEnd = `${year[0]}${Number(year[1]) + 1}00`;
+
+    century = Math.round(centuryEnd / 100);
+  }
+
+  return century;
 }
 
 console.log(century(1));

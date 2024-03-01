89214d672b75d3aa930450d2188bd0391e4a0b70
Abito21 <abidjuliant@gmail.com>
2024-03-02 05:45:00 +0700

    Logic code return string using template literals

---
+++ javascript/22-returnSting.js
@@ -0,0 +1,17 @@
+/*
+Problem :
+Make a function that will return a greeting statement that uses an input; your program should return, 
+"Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+// First Logic : using template literals
+function greet(name){
+    //your code here
+    return `Hello, ${name} how are you doing today?`
+}
+
+console.log(greet("Abid"))
+console.log(greet("Juliant"))
+console.log(greet("Indraswara"))

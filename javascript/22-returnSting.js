fe6e05fdcd06920e9c449a2bda0b93d4bca47bdf
Abito21 <abidjuliant@gmail.com>
2024-03-02 05:51:08 +0700

    Fourth ogic code return string using replace() function

--- javascript/22-returnSting.js
+++ javascript/22-returnSting.js
@@ -19,9 +19,14 @@ Make a function that will return a greeting statement that uses an input; your p
 // }
 
 // Third Logic : using ASCII code
+// function greet(name){
+//     //your code here
+//     return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`
+// }
+
+// Fourth Logic : using replace() function
 function greet(name){
-    //your code here
-    return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`
+    return "Hello, <name> how are you doing today?".replace("<name>", name);
 }
 
 console.log(greet("Abid"))

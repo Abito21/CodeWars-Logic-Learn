d11dbae709f9c3c435c373c15154c6b13fb2ca0d
Abito21 <abidjuliant@gmail.com>
2024-03-02 05:48:16 +0700

    Third logic code return string using ASCII code

--- javascript/22-returnSting.js
+++ javascript/22-returnSting.js
@@ -13,9 +13,15 @@ Make a function that will return a greeting statement that uses an input; your p
 // }
 
 // Second Logic : using plus string
+// function greet(name){
+//     //your code here
+//     return "Hello, " + name + " how are you doing today?"
+// }
+
+// Third Logic : using ASCII code
 function greet(name){
     //your code here
-    return "Hello, " + name + " how are you doing today?"
+    return `\x48\x65\x6c\x6c\x6f\x2c\x20${name}\x20\x68\x6f\x77\x20\x61\x72\x65\x20\x79\x6f\x75\x20\x64\x6f\x69\x6e\x67\x20\x74\x6f\x64\x61\x79\x3f`
 }
 
 console.log(greet("Abid"))

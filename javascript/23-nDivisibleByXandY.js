2f7be74e6d70dcdc6f73632fc0b62966f4ce2448
Abito21 <abidjuliant@gmail.com>
2024-03-05 05:32:44 +0700

    Third logic code is n divisible by x and y using ternary operator

--- javascript/23-nDivisibleByXandY.js
+++ javascript/23-nDivisibleByXandY.js
@@ -19,8 +19,13 @@ Examples:
 // }
 
 // Second Logic : using return condition
+// function isDivisible(n, x, y) {
+//     return n % x === 0 && n % y === 0
+// }
+
+// Third Logic : using ternary operation
 function isDivisible(n, x, y) {
-    return n % x === 0 && n % y === 0
+    return (n % x === 0 && n % y === 0) ? true : false
 }
 
 console.log(isDivisible(3,3,4))

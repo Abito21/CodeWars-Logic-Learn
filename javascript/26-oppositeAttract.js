0f51a0c96fbc385070b65d5f53c5007ddd80a994
Abito21 <abidjuliant@gmail.com>
2024-03-15 13:32:24 +0700

    Second logic code using simple if else condition

--- javascript/26-oppositeAttract.js
+++ javascript/26-oppositeAttract.js
@@ -5,16 +5,22 @@ Timmy & Sarah think they are in love, but around where they live, they will only
 Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 */
 
-// First Logic : using if else condition
+// // First Logic : using if else condition
+// function lovefunc(flower1, flower2){
+//     // moment of truth
+//     if (flower1 % 2 == 0 && flower2 % 2 != 0) {
+//         return true
+//     } else if (flower2 % 2 == 0 && flower1 % 2 != 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// Second Logic : using simple if else condition
 function lovefunc(flower1, flower2){
     // moment of truth
-    if (flower1 % 2 == 0 && flower2 % 2 != 0) {
-        return true
-    } else if (flower2 % 2 == 0 && flower1 % 2 != 0) {
-        return true
-    } else {
-        return false
-    }
+    return flower1 % 2 !== flower2 % 2;
 }
 
 console.log(lovefunc(1,4))
